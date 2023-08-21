0d55d93063b5d08e95367b7084bb1ca0b5916432
tolossamuel <tolossamuel1@gmail.com>
2023-08-22 01:51:49 +0300

    alx team work

--- _printf.c
+++ _printf.c
@@ -1,43 +1,50 @@
 #include "main.h"
+#include <limits.h>
+#include <stdio.h>
 
 /**
- * _printf - functions
- * @format: pointer
- * Return: number
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i, s_len;
-	unsigned int count = 0;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	va_list list;
+	register int count = 0;
 
-	va_start(list, format);
-
-	for (i = 0; format[i] != '\0'; i++)
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		if (format[i] != '%')
+		if (*p == '%')
 		{
-			_putchar(format[i]);
-			count += 1;
-		}
-		else if (format[i] == '%' && format[i + 1] == 'c')
-		{
-			_putchar(va_arg(list, int));
-			i++;
-		}
-		else if (format[i + 1] == 's')
-		{
-			s_len = len(va_arg(list, char *));
-			count += s_len;
-			i++;
-		}
-		else if (format[i + 1] == '%')
-		{
-			_putchar('%');
-			i++;
-		}
-		va_end(list);
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
+	_putchar(-1);
+	va_end(arguments);
 	return (count);
+
 }
